457bca824fda832d5d88ffe6064bddcd74b0c3f4
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-03-09 18:43:55 -0500

    ventanas agregadas
    agregada ventana principal y ventana de agregar estudiante

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from View.main_window import Ui_MainWindow
-from View.add_student_window import Ui_add_student
+from View.main_window_view import Ui_MainWindow
+from View.add_student_view import Ui_Add_student
 from Controller.student_control import add_student_control, get_student_list
 from PyQt5.QtCore import QStringListModel
 
@@ -12,7 +12,7 @@ class MainWindow(QMainWindow):
         self.ui_main = Ui_MainWindow()
         self.ui_main.setupUi(self)
         self.show_student_list()
-        self.ui_main.pushButton.clicked.connect(self.open_add_student)
+        self.ui_main.pushButton_add_estudiante.clicked.connect(self.open_add_student)
         
     def open_add_student(self):
         self.add_stu_win = AddStudent()
@@ -23,20 +23,21 @@ class MainWindow(QMainWindow):
         new_list = []
         for st in student_list:
             new_list.append(st.apellidos+' '+st.nombres+' '+st.cedula)
-        list_model = QStringListModel()
-        list_model.setStringList(new_list)
-        self.ui_main.listView.setModel(list_model)
+        # list_model = QStringListModel()
+        # list_model.setStringList(new_list)
+        self.ui_main.listWidget_estudiantes.addItems(new_list)
+        # self.ui_main.listWidget_estudiantes.setModel(list_model)
 
 class AddStudent(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui_addstu = Ui_add_student()
+        self.ui_addstu = Ui_Add_student()
         self.ui_addstu.setupUi(self)
-        self.ui_addstu.pushButton_2.clicked.connect(self.close)
-        self.ui_addstu.pushButton.clicked.connect(self.add_student)
+        self.ui_addstu.pushButton_cancel.clicked.connect(self.close)
+        
         
     def add_student(self):
-        flag = add_student_control(self.ui_addstu.lineEdit.text(), self.ui_addstu.lineEdit_2.text(), self.ui_addstu.lineEdit_3.text())
+        flag = add_student_control()
         if flag:
             print('estudiante agregado')
             
