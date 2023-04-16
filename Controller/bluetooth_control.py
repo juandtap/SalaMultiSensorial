fd14bf5135b395eddd53fc2e0287c2bef77a6bce
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-16 15:17:26 -0500

    cambio de version de python 3.8 -> 3.9

--- Controller/bluetooth_control.py
+++ Controller/bluetooth_control.py
@@ -1,69 +1,38 @@
 # este modulo es para obtener el puerto COM al que esta conectada
 # la PC por bluetooth
-# !!!!aun no se prueba !!!!
-# import bluetooth
-
-# target_name = "HC-05"
-# target_address = None
-
-# # Buscamos dispositivos Bluetooth cercanos
-# nearby_devices = bluetooth.discover_devices()
-
-# # Buscamos el dispositivo con el nombre especificado
-# for bdaddr in nearby_devices:
-#     if target_name == bluetooth.lookup_name( bdaddr ):
-#         target_address = bdaddr
-#         break
-
-# # Si encontramos el dispositivo, buscamos el puerto COM al que está conectado
-# if target_address is not None:
-#     services = bluetooth.find_service(address=target_address)
-#     for svc in services:
-#         if svc['name'] == "Serial Port":
-#             port = svc['port']
-#             print(f"El dispositivo {target_name} está conectado al puerto COM {port}")
-# else:
-#     print(f"No se pudo encontrar el dispositivo {target_name}")
-
-# import bluetooth
-
-# # Busca todos los dispositivos Bluetooth cercanos
-# devices = bluetooth.discover_devices()
-
-# # Busca los puertos COM a los que están conectados los dispositivos Bluetooth
-# for device in devices:
-#     print(f"Buscando puertos COM para el dispositivo: {device}")
-#     services = bluetooth.find_service(address=device)
-#     for service in services:
-#         if service["service-id"] == "Serial Port":
-#             print(f"Dispositivo Bluetooth encontrado en el puerto COM: {service['port']}")
 
 
-# import serial.tools.list_ports
-
-# ports = serial.tools.list_ports.comports()
-
-# for port in ports:
-#     print(port.pid)
-    
+import serial.tools.list_ports
 import bluetooth
 
-addr = '00:22:03:01:8A:12'  # Dirección MAC del HC-05 4C:34:88:B5:29:25
-port = None
+def find_port(mac_address):
+    # Enumera todos los puertos seriales disponibles en el sistema
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        # Obtiene el nombre del dispositivo del puerto serial
+        device = p.device
+        # Busca la dirección MAC del dispositivo Bluetooth que corresponde al puerto serial
+        try:
+            device_info = bluetooth.lookup_name(mac_address, timeout=5)
+            # if device_info is not None and device_info == p.hwid:
+            if device_info is not None:
+                return device
+        except:
+            pass
+    # Devuelve None si no se encuentra ningún puerto correspondiente a la dirección MAC
+    return None
 
-# Busca los servicios disponibles en el dispositivo remoto
-services = bluetooth.find_service(address=addr)
-
-# Itera a través de los servicios buscando el puerto RFCOMM
-for s in services:
-    if s['protocol'] == 'RFCOMM':
-        port = s['port']
-        break
-
-if port:
-    print('El HC-05 está conectado al puerto COM', port)
+mac_address = '00:22:03:01:8A:12' # reemplazar con la dirección MAC de tu modulo HC-05
+port = find_port(mac_address)
+if port is not None:
+    print(f"El modulo HC-05 esta conectado al puerto {port}")
 else:
-    print('No se encontró el puerto COM del HC-05')
+    print("No se encuentra el puerto para el modulo HC-05")
+
+devices = bluetooth.discover_devices()
+for device in devices:
+    print(device)
+
 
 
 
