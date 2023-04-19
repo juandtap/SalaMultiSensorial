03479d86ee4b646aae02a562de574cab7395dbf9
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-19 12:05:25 -0500

    nuevo vumetro (no funciona)

--- View/module_components_2.py
+++ View/module_components_2.py
@@ -4,9 +4,13 @@ import serial, time, threading
 sys.path.append(".")
 from datetime import datetime
 from datetime import time as time2
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox, QGraphicsScene, QGraphicsRectItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox, QGraphicsScene, QGraphicsRectItem, QGraphicsProxyWidget
 from PyQt5.QtCore import Qt, QDate, QTimer, QTime, QThread, pyqtSignal, QRectF
 from PyQt5.QtGui import QPixmap, QPen, QBrush, QColor
+from PyQt5.QtCore import QTimer
+import pyqtgraph as pg
+import random
+
 from View.module_vumeter_view import Ui_Form_modulo_vumetro
 from Controller.session_control import add_sesion_module, get_sesion_by_id
 from Model.model import Sesion, ModuloGrafomotricidad
@@ -23,12 +27,45 @@ class ModuleVumeter(QWidget):
         self.ui_vum.label_text_status.setHidden(True)
         self.ui_vum.label_conn_status.setHidden(True)
         
-        self.draw_vumeter_level()
+        #self.draw_vumeter_level()
         
-        self.ui_vum.pushButton_start.clicked.connect(self.set_level)
+        # Crea un objeto GraphicsLayoutWidget y agrega un PlotWidget
+        self.graphics_layout = pg.GraphicsLayoutWidget()
+        self.graphics_layout.setAutoPanels(False)
+        self.plot_widget = self.graphics_layout.addPlot()
+        self.plot_widget.setBackground(None)
+        self.plot_data = [0]  # Inicialmente, solo hay un punto en el plot
+        self.plot_curve = self.plot_widget.plot(self.plot_data)
+
+        # Crea un objeto QGraphicsProxyWidget y agrega el GraphicsLayoutWidget a él
+        proxy = QGraphicsProxyWidget()
+        proxy.setWidget(self.graphics_layout)
         
+        self.scene = QGraphicsScene()
+        self.scene.addItem(proxy)
         
+        self.ui_vum.graphicsView.setScene(self.scene)
+        
+        self.ui_vum.pushButton_start.clicked.connect(self.add_data)
+        
+        proxy_scene_pos = proxy.mapFromScene(0,0)
+        button_scene_pos = self.ui_vum.graphicsView.mapFromScene(proxy_scene_pos)
+        self.ui_vum.pushButton_start.move(button_scene_pos.x(), button_scene_pos.y() - 30)
+        #self.ui_vum.graphicsView.scene().addWidget(self.ui_vum.pushButton_start)
+        
+        # Configura un temporizador para actualizar el plot cada cierto tiempo
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(50)  # Actualiza cada 50ms
     
+    def add_data(self):
+        # Agrega un valor aleatorio al plot_data
+        self.plot_data.append(random.uniform(0, 10))
+        
+    def update_plot(self):
+        # Actualiza el plot_curve con los nuevos datos
+        self.plot_curve.setData(self.plot_data)
+
     def set_module_images(self):
         pixmap1 = QPixmap("Assets/modulo_2_vumetro.png")
         self.ui_vum.label_module_image.setPixmap(
