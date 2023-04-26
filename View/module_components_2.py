91560176dbea4028a934f2eb7edc16685c0e4b4d
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-25 21:58:27 -0500

    agregado plot al modulo vumetro

--- View/module_components_2.py
+++ View/module_components_2.py
@@ -6,12 +6,13 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 sys.path.append(".")
 from datetime import datetime
 from datetime import time as time2
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox, QGraphicsScene, QGraphicsRectItem, QGraphicsProxyWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox, QGraphicsScene, QGraphicsRectItem, QGraphicsProxyWidget, QVBoxLayout
 from PyQt5.QtCore import Qt, QDate, QTimer, QTime, QThread, pyqtSignal, QRectF
 from PyQt5.QtGui import QPixmap, QPen, QBrush, QColor
 from PyQt5.QtCore import QTimer
 import pyqtgraph as pg
-import random
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import numpy as np
 
 from View.module_vumeter_view import Ui_Form_modulo_vumetro
 from Controller.session_control import add_sesion_module, get_sesion_by_id
@@ -31,9 +32,32 @@ class ModuleVumeter(QWidget):
         
         #self.draw_vumeter_level()
         #self.set_level()
+        layout = QVBoxLayout()
+        self.plot_vum = PlotWidget()
+        layout.addWidget(self.plot_vum)
+        self.ui_vum.plot_widget.setLayout(layout)
         
+        self.ui_vum.pushButton_start.clicked.connect(self.agregar_datos)
         
+         # Establecer los parámetros de la gráfica
+        self.data = []  # Lista para almacenar los datos
+        self.time = []  # Lista para almacenar los tiempos
+        
+         # Crear un QTimer que actualice la gráfica cada 100 ms
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.actualizar_grafica)
+        self.timer.start(100)
 
+        # Crear una variable para el tiempo
+        self.tiempo = 0
+        
+    def agregar_datos(self):
+        dato = random.randint(0,100)
+        self.tiempo =+ 1
+        self.plot_vum.update_plot(dato, self.tiempo)
+        
+    def actualizar_grafica(self):
+        self.plot_vum.canvas.draw()
 
     def set_module_images(self):
         pixmap1 = QPixmap("Assets/modulo_2_vumetro.png")
@@ -96,4 +120,42 @@ class ModuleVumeter(QWidget):
         
         for i in range(last_index, last_index - level, -1):
             self.scene.items()[i].setVisible(True)
-           
+
+class PlotWidget(QWidget):
+    def __init__(self, parent=None):
+        super(PlotWidget, self).__init__(parent)
+
+        # Crear una figura de Matplotlib y un objeto de plot
+        self.figure = plt.figure()
+        self.axes = self.figure.add_subplot(111)
+
+        # Crear un objeto de canvas de Matplotlib
+        self.canvas = FigureCanvas(self.figure)
+
+        # Agregar el canvas a este widget
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+        # Crear una lista para almacenar los datos que se graficarán
+        self.data = []
+        self.time = []
+
+    def update_plot(self, new_data, new_time):
+        # Agregar los nuevos datos y el nuevo tiempo a las listas de datos y tiempo
+        #self.data.append(new_data)
+        #self.time.append(new_time)
+        self.data = [3, 6, 7, 10, 20, 30, 59, 70, 40, 50]
+        self.time = [1,2,3,4,5,6,7,8,9,10]
+
+        # Limitar la cantidad de puntos mostrados en la gráfica a 100
+        if len(self.data) > 100:
+            self.data.pop(0)
+            self.time.pop(0)
+
+        # Limpiar el plot y graficar los datos
+        self.axes.clear()
+        self.axes.plot(self.time, self.data)
+
+        # Actualizar el canvas
+        self.canvas.draw()
