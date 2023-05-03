754d6e37e7df7cddb5a106ffd78108bb425a3588
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-03 11:45:08 -0500

    cambios ventana reportes

--- View/components_2.py
+++ View/components_2.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(".")
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QComboBox, QFileDialog, QAbstractItemView, QTableWidgetItem, QHeaderView
 from PyQt5.QtCore import Qt, QDate
-from PyQt5.QtGui import  QStandardItemModel  
+from PyQt5.QtGui import  QStandardItemModel , QColor, QBrush, QFont
 from datetime import time, date
 
 from View.student_report_view import Ui_Form_student_report
@@ -19,12 +19,18 @@ class StudentReport(QWidget):
         
         self.load_info_student()
        
+       
+        self.font_table = QFont('Arial',10)
+        self.font_table.setUnderline(True)
+        
         # variable temporal borrar despues
         self.download = 'ver modulos'
         
         self.set_table()
         
         self.load_table()   
+        
+        self.ui_rep.tableWidget.cellClicked.connect(self.show_reports)
         
        
     def set_table(self):
@@ -67,8 +73,10 @@ class StudentReport(QWidget):
                 self.ui_rep.tableWidget.setItem(i, 1, QTableWidgetItem(str(sesion.fecha)))
                 self.ui_rep.tableWidget.setItem(i, 2, QTableWidgetItem(str(sesion.hora_inicio)))
                 self.ui_rep.tableWidget.setItem(i, 3, QTableWidgetItem(str(sesion.hora_fin)))
-                self.ui_rep.tableWidget.setItem(i, 4, QTableWidgetItem(self.download))
-        
+                self.ui_rep.tableWidget.setItem(i, 4, QTableWidgetItem(self.download+"-"+(str(sesion.id))))
+                self.ui_rep.tableWidget.item(i,4).setFont(self.font_table)
+                
+                
     
     def load_info_student(self):
         self.ui_rep.label_cedula.setText(self.student.cedula)
@@ -76,4 +84,12 @@ class StudentReport(QWidget):
         self.ui_rep.label_nombres.setText(self.student.nombres)         
         
         
-
+    def show_reports(self, row, column):
+        # la ultima columna para abrir la ventana donde se muestran el resto de tablas
+        show_modules_pos = 4
+        item = self.ui_rep.tableWidget.item(row, 4)
+        id_ses = item.text().split('-')[-1]
+        print("id sesion : "+id_ses)
+        
+        # recupera la sesion y muestra la info de los modulos
+        
