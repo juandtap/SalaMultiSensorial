895da544edac6691e6796c08b9a836c02d8638f3
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-04 10:56:54 -0500

    Update bluetooth_control.py

--- Controller/bluetooth_control.py
+++ Controller/bluetooth_control.py
@@ -7,17 +7,20 @@ import bluetooth
 
 mac_hc05_testing = '00:22:03:01:8A:12'
 mac_hc05_grafomotricidad = '00:22:03:01:7D:DC'
+
 addr = mac_hc05_testing
 
 # Conectar al dispositivo
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 sock.connect((addr, 1))
 
 # Enviar datos al dispositivo
 sock.send('Hola, HC05!')
 
+while True:
 # Recibir datos del dispositivo
-data = sock.recv(1024)
+    data = sock.recv(1024)
+    print(data)
 
 # Cerrar la conexión
 sock.close()
