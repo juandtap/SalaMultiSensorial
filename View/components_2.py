9389c3de24be93837cf31f1f2443df6f6e6c1bfc
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-03 22:33:34 -0500

    boton de regresar a tabla principal terminado

--- View/components_2.py
+++ View/components_2.py
@@ -1,8 +1,9 @@
 # este modulo es para la ventana de reportes del estudiante
 
 import sys
+from PyQt5 import QtCore
 sys.path.append(".")
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QComboBox, QFileDialog,QTableWidget, QAbstractItemView, QTableWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QSizePolicy, QVBoxLayout, QLabel, QComboBox, QFileDialog,QTableWidget, QAbstractItemView, QTableWidgetItem, QHeaderView
 from PyQt5.QtCore import Qt, QDate
 from PyQt5.QtGui import  QStandardItemModel , QColor, QBrush, QFont
 from datetime import time, date
@@ -18,25 +19,34 @@ class StudentReport(QWidget):
         self.student = student
         
         self.load_info_student()
+        
        
        
         # esconder label de regresar
         self.ui_rep.label_back.setHidden(True)
-       
+        
+        # fuente para los enlaces
         self.font_table = QFont('Arial',10)
         self.font_table.setUnderline(True)
+        
+        # fuente para las label de tablas
+        self.font = QFont('Arial', 10)
+        self.font.setBold(True)
+        
+        # fuente label back
+        self.font_in = QFont('Arial',10,1,True)
+        self.font_in.setBold(True)
+        self.font_in.setUnderline(True)
+        self.font_out = QFont('Arial',10,1,True)
+        self.font_out.setBold(False)
+        self.font_out.setUnderline(True)
+        
         
         # variable temporal borrar despues
         self.download = 'ver modulos'
         
-        self.table_reports = QTableWidget()
-        self.area_table = QWidget()
-        area_table_layout = QVBoxLayout()
-        self.area_table.setLayout(area_table_layout)
-        
-        area_table_layout.addWidget(self.table_reports)
-        
-        self.ui_rep.scrollArea.setWidget(self.area_table)
+        self.ui_rep.scrollArea.setWidgetResizable(True)
+
         
         self.set_table()
         
@@ -44,8 +54,36 @@ class StudentReport(QWidget):
         
         self.table_reports.cellClicked.connect(self.show_reports)
         
+        # evento label: Regresar
+       
+        self.ui_rep.label_back.mousePressEvent = self.back_to
+        self.ui_rep.label_back.enterEvent = self.mouse_over
+        self.ui_rep.label_back.leaveEvent = self.mouse_out
+        
+    
+    def mouse_over(self, event):
+        self.ui_rep.label_back.setFont(self.font_in)
+    
+    def mouse_out(self,event):
+        self.ui_rep.label_back.setFont(self.font_out)
+        
+    
+    
        
     def set_table(self):
+        
+        self.table_reports = QTableWidget()
+        self.area_table = QWidget()
+        self.area_table_layout = QVBoxLayout()
+        self.area_table.setLayout(self.area_table_layout)
+        
+        self.area_table_layout.addWidget(self.table_reports)
+        
+        #self.ui_rep.scrollArea.setMinimumWidth(1100)
+        #self.ui_rep.scrollArea.setMinimumHeight(400)
+        self.ui_rep.scrollArea.setWidgetResizable(True)
+        self.ui_rep.scrollArea.setWidget(self.area_table)
+        
         self.table_reports.verticalHeader().setVisible(False)
         # defino las columnas de la tabla
         self.table_reports.setColumnCount(5)
@@ -88,7 +126,15 @@ class StudentReport(QWidget):
                 self.table_reports.setItem(i, 4, QTableWidgetItem(self.download+"-"+(str(sesion.id))))
                 self.table_reports.item(i,4).setFont(self.font_table)
                 
-                
+        
+    def back_to(self, event):
+        print("se hizo click en la label back")
+        self.ui_rep.scrollArea.takeWidget().deleteLater()
+        self.set_table()
+        self.load_table()
+        self.ui_rep.scrollArea.setWidget(self.area_table)
+        self.table_reports.cellClicked.connect(self.show_reports)
+        self.ui_rep.label_back.setHidden(True)  
     
     def load_info_student(self):
         self.ui_rep.label_cedula.setText(self.student.cedula)
@@ -102,33 +148,47 @@ class StudentReport(QWidget):
         item = self.table_reports.item(row, 4)
         id_ses = item.text().split('-')[-1]
         print("id sesion : "+id_ses)
+        
+        self.ui_rep.label_back.setHidden(False)
+        
         self.ui_rep.scrollArea.takeWidget().deleteLater()
-        module_tables = ModuleReport(id_ses)
-        self.ui_rep.scrollArea.setWidget(module_tables)
+       
+        
+        self.ui_rep.scrollArea.setWidget(self.load_modules_tables(id_ses))
+       
         # recupera la sesion y muestra la info de los modulos
         # crear QWidget para mostrar la info de los modulos
         # probar conectarse, enviar y recibir datos por bluetooth usando MAC y no el puerto
-        
-# esta clase carga las tablas de la sesion
-# el onjeto sesion tiene las listas de cada modulo
-# en cada lista se guarda objetos de tipo ModuloGrafo, ModuloVumeter, ....
 
-class ModuleReport(QWidget):
-    def __init__(self, id_sesion):
-        super().__init__()
+    def load_modules_tables(self, id_ses):
+        container = QWidget()
         
         layout = QVBoxLayout()
-        self.setLayout(layout)
+        container.setLayout(layout)
         
         # se recupera los datos de los modulos de la sesion
-        # una lista por cada modulo
+        # la sesion tiene una lista por cada modulo
         
-        #Modulo de grafomotricidad
-        grafomotricidad_list = get_sesion_by_id(id_sesion).modulos_grafomotricidad
+        self.sesion_selected = get_sesion_by_id(id_ses)
         
+        # muestra el id de la sesion y la fecha
+        label_info_sesion = QLabel('Sesion '+str(self.sesion_selected.id)+" Fecha: "+str(self.sesion_selected.fecha))
+        label_info_sesion.setFont(self.font)
+        layout.addWidget(label_info_sesion)
+        
+        # muestra el nombre del primer modulo
+        label_grafomotricidad = QLabel("Modulo Grafomotricidad")
+        label_grafomotricidad.setFont(self.font)
+        layout.addWidget(label_grafomotricidad)
+        
+         # guarda los datos del Modulo de grafomotricidad en una lista y los muestra en una tabla
+        grafomotricidad_list = self.sesion_selected.modulos_grafomotricidad
+        
+        # crea la tabla donde muestra los datos del modulo de grafomotricidad
         self.table_grafomotricidad = QTableWidget()
         layout.addWidget(self.table_grafomotricidad)
         
+        # configura la tabla
         self.table_grafomotricidad.verticalHeader().setVisible(False)
         # defino las columnas de la tabla
         self.table_grafomotricidad.setColumnCount(4)
@@ -159,9 +219,108 @@ class ModuleReport(QWidget):
             self.table_grafomotricidad.setItem(i, 1, QTableWidgetItem(str(module.figura)))
             self.table_grafomotricidad.setItem(i, 2, QTableWidgetItem(str(module.tiempo_tomado)))
             self.table_grafomotricidad.setItem(i, 3, QTableWidgetItem(str(module.resultado)))
+        
+        # Modulo Vumetro
+        # repite el proceso pero para el modulo vumetro
+        # se usan los datos del modulo de grafomotricidad por el momento
+        
+        label_vumetro = QLabel("Modulo Vumetro")
+        label_vumetro.setFont(self.font)
+        layout.addWidget(label_vumetro)
+        
+        self.table_vumetro = QTableWidget()
+        layout.addWidget(self.table_vumetro)
+        
+        self.table_vumetro.verticalHeader().setVisible(False)
+        # defino las columnas de la tabla
+        self.table_vumetro.setColumnCount(4)
+        self.table_vumetro.setHorizontalHeaderLabels(
+            [
+                'id',
+                'Figura',
+                'Tiempo Tomado',
+                'Resultado',
+                
+            ]
+        )
+        
+        self.table_vumetro.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table_vumetro.setSelectionBehavior(QAbstractItemView.SelectRows)
+        
+        # hace que la ultima columna ocupe el espacio restante de la tabla
+        self.table_vumetro.horizontalHeader().setStretchLastSection(True)
+        
+        # este codigo hace que las columnas  no sean redimensionables
+        header = self.table_vumetro.horizontalHeader()
+        for i in range(header.count()):
+            header.setSectionResizeMode(i,QHeaderView.Fixed)
             
+        for i, module in enumerate(grafomotricidad_list):
+            self.table_vumetro.insertRow(i)
+            self.table_vumetro.setItem(i, 0, QTableWidgetItem(str(module.id)))
+            self.table_vumetro.setItem(i, 1, QTableWidgetItem(str(module.figura)))
+            self.table_vumetro.setItem(i, 2, QTableWidgetItem(str(module.tiempo_tomado)))
+            self.table_vumetro.setItem(i, 3, QTableWidgetItem(str(module.resultado)))
+            
+        # Modulo Iluminacion
+        
+        label_iluminacion = QLabel("Modulo Iluminacion")
+        label_iluminacion.setFont(self.font)
+        layout.addWidget(label_iluminacion)
+        
+        self.table_iluminacion = QTableWidget()
+        layout.addWidget(self.table_iluminacion)
+        
+        self.table_iluminacion.verticalHeader().setVisible(False)
+        # defino las columnas de la tabla
+        self.table_iluminacion.setColumnCount(4)
+        self.table_iluminacion.setHorizontalHeaderLabels(
+            [
+                'id',
+                'Figura',
+                'Tiempo Tomado',
+                'Resultado',
+                
+            ]
+        )
+        
+        self.table_iluminacion.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table_iluminacion.setSelectionBehavior(QAbstractItemView.SelectRows)
+        
+        # hace que la ultima columna ocupe el espacio restante de la tabla
+        self.table_iluminacion.horizontalHeader().setStretchLastSection(True)
+        
+        # este codigo hace que las columnas  no sean redimensionables
+        header = self.table_iluminacion.horizontalHeader()
+        for i in range(header.count()):
+            header.setSectionResizeMode(i,QHeaderView.Fixed)
+            
+        for i, module in enumerate(grafomotricidad_list):
+            self.table_iluminacion.insertRow(i)
+            self.table_iluminacion.setItem(i, 0, QTableWidgetItem(str(module.id)))
+            self.table_iluminacion.setItem(i, 1, QTableWidgetItem(str(module.figura)))
+            self.table_iluminacion.setItem(i, 2, QTableWidgetItem(str(module.tiempo_tomado)))
+            self.table_iluminacion.setItem(i, 3, QTableWidgetItem(str(module.resultado)))
+        
+        
+        label_pictograma = QLabel("Modulo Pictogramas")
+        label_pictograma.setFont(self.font)
+        layout.addWidget(label_pictograma)
+        
+           
+
+        return container
+       
+        
+        
+        
+        
+        
+       
             
         
         
         
+        
+       
     
