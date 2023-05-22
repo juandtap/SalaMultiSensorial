41fb33dab70d7d8f8285056c6ae8418570698c7e
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-22 13:00:32 -0500

    PDF generado a partir de HTML terminado
    ver errores en los estilos y carga de imagenes

--- Controller/report_control.py
+++ Controller/report_control.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(".")
 # import os
 # os.add_dll_directory(r'C:\\msys64\\mingw64\\bin')
-# from weasyprint import HTML
+from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 from Controller.student_control import get_student_by_id
 from Controller.session_control import get_sesion_by_id
@@ -74,3 +74,8 @@ with open('SessionReports/reporte1.html', 'w') as file:
 
 # Convertir el archivo HTML a PDF
 ## HTML(filename='/SessionReports/reporte1.html').write_pdf('reporte1.pdf')
+# Crea un objeto Document con el HTML renderizado
+doc = HTML(string=output)
+
+# Guarda el archivo PDF resultante
+doc.write_pdf('SessionReports/pdfreport1.pdf')
